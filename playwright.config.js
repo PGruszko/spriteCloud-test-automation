10427107a01e84792ec6d5008541075a761f038a
Jesus Lorca Luque <paulinagru94@gmail.com>
2025-10-26 20:57:59 +0100

    upload report with playwright

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [
+    ["list"], // You can combine multiple reporters
+    [
+      "playwright-ctrf-json-reporter",
+      {
+        outputFile: "ctrf-report-playwright.json", // Optional: Output file name. Defaults to 'ctrf-report.json'.
+        outputDir: "./ctrf",
+      },
+    ],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
